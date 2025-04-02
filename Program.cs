fa31f63a0a4a278ed35464f26eee356552c31168
NotNebu <nebula-p@outlook.com>
2025-04-02 20:40:18 +0200

    Testet forbindelse blandt services

--- Program.cs
+++ Program.cs
@@ -2,6 +2,19 @@ using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Tvinger http for at teste Curl :~D
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5107); // HTTP
+});
+
+// Forbindelse til gRPC User Klienten
+builder.Services.AddGrpcClient<Greeter.GreeterClient>(o =>
+{
+    o.Address = new Uri("http://localhost:5001"); 
+});
+builder.Services.AddControllers();
+
 // Load config from appsettings.json
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
